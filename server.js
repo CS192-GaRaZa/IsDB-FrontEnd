25f910467209cc1bfdce8c9070c0bdf7bf824e66
Zach <zjam.ubias@gmail.com>
2017-03-23 07:52:15 +0800

    Create node webserver for deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.set('port', process.env.PORT || 12121);
+app.use(express.static(__dirname + '/webapp'));
+app.use(logger('dev'));
+
+app.get('/', (_, res) => {
+  res.sendFile('webapp/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+})
