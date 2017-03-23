5e11d4ba066d8ed74daafa2331735db218a3801d
Zach <zjam.ubias@gmail.com>
2017-03-23 12:33:53 +0800

    Log-in page now makes an ajax call to the server

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,12 +1,43 @@
-
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
+  'sap/ui/core/mvc/Controller',
+  'sap/ui/model/json/JSONModel'
 ], function LoginController(
-  Controller
+  Controller,
+  JSONModel
 ) {
-  "use strict";
-  return Controller.extend("cmsfrontend.controller.Login", {
+  'use strict';
+  return Controller.extend('cmsfrontend.controller.Login', {
+
     onInit: function onInit() {
+      var oLoginModel = new JSONModel({
+        user: {
+          email: '',
+          password: ''
+        }
+      });
+
+      this.getView().setModel(oLoginModel);
+    },
+
+    onPressLogOn: function onPressLogOn(oControlEvent) {
+      var oLoginModel = this.getView().getModel();
+      console.log(oLoginModel.getJSON());
+
+      $.ajax({
+        url: 'https://isdb-cms-api.herokuapp.com/api/v1/sessions/',
+        method: 'POST',
+        data: oLoginModel.getJSON(),
+        contentType: 'application/json',
+        success: function(data, textStatus, jqXHR) {
+          Cookies.set('isdb', data);
+        },
+        error: function(xhr, status, errorThrown) {
+          console.log('ERROR POSTING REQUEST');
+          console.log('xhr: ', xhr);
+          console.log('status: ', status);
+          console.log('errorThrown: ', errorThrown);
+        }
+      });
     }
   });
 });
