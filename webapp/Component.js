f83afaf37837b8e2532d4f5f619e8533ec98031c
Zach <zjam.ubias@gmail.com>
2017-03-23 09:48:18 +0800

    Convert indentation to spaces

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,19 +1,19 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/odata/ODataModel",
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/odata/ODataModel",
 ],function(UIComponent,ODataModel){
-	"use strict";
-	return UIComponent.extend("cmsfrontend.Component",{
-		metadata : {
-			manifest : "json"
-		},
-		init : function(){
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
+  "use strict";
+  return UIComponent.extend("cmsfrontend.Component",{
+    metadata : {
+      manifest : "json"
+    },
+    init : function(){
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments);
 
-			// create the views based on the url/hash
-			this.getRouter().initialize();
+      // create the views based on the url/hash
+      this.getRouter().initialize();
 
-		}
-	});
+    }
+  });
 });
