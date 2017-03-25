ddd49ed08c4687cbbe1e8c4d7ba11b2e424ce441
Zach <zjam.ubias@gmail.com>
2017-03-25 20:33:51 +0800

    Add POST functionality to Consulting Firm sign-up

--- webapp/controller/consultingfirm/SignUp.controller.js
+++ webapp/controller/consultingfirm/SignUp.controller.js
@@ -1,16 +1,66 @@
 sap.ui.define([
-  'sap/ui/core/mvc/Controller'
+  'sap/ui/core/mvc/Controller',
+  "sap/ui/model/json/JSONModel"
 ], function LoginController(
-  Controller
+  Controller,
+  JSONModel
 ) {
   'use strict';
   return Controller.extend('cmsfrontend.controller.consultingfirm.SignUp', {
 
     onInit: function onInit() {
+      var oModel = new JSONModel({
+        "user": {
+          "cf_name": "",
+          "email": "",
+          "password": "",
+          "kind": "consulting_firm",
+          "role": "consulting_firm"
+        }
+      });
+      this.getView().setModel(oModel);
     },
 
     onPressConfirm: function onPressConfirm() {
-      console.log("Signed Up");
+      var _this = this;
+      var oModel = this.getView().getModel();
+
+      $.ajax({
+        url: 'https://isdb-cms-api.herokuapp.com/api/v1/users/',
+        method: 'POST',
+        data: oModel.getJSON(),
+        contentType: 'application/json',
+        success: _this._onSuccess.bind(_this),
+        error: function(xhr, status, errorThrown) {
+          console.log('ERROR POSTING REQUEST');
+          console.log('xhr: ', xhr);
+          console.log('status: ', status);
+          console.log('errorThrown: ', errorThrown);
+        }
+      });
+    },
+
+    _onSuccess: function _onSuccess(data, textStatus, jqXHR) {
+      var _this = this;
+      var oView = this.getView();
+      var oDialog = oView.byId("idAccountCreated");
+
+      if (!oDialog) {
+        var oFragmentController = {
+          onPressOk: function onPressOk() {
+            oDialog.close();
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(_this);
+            oRouter.navTo("login");
+          }
+        };
+
+        oDialog = sap.ui.xmlfragment(oView.getId(),
+            "cmsfrontend.view.fragments.AccountCreated",
+            oFragmentController);
+        oView.addDependent(oDialog);
+      }
+
+      oDialog.open();
     }
 
   });
