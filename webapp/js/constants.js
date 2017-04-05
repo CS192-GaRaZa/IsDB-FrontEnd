69b4560f21e41460118c7b00746d2d33d0886449
Zach <zjam.ubias@gmail.com>
2017-04-05 22:25:07 +0800

    Create global constants and utils file
    The constants can be accessed through appConstants.<CONSTANT> while
    the utility functions can be accessed through appUtils.<util_name>.

---
+++ webapp/js/constants.js
@@ -0,0 +1,10 @@
+/**Declare all app constants in this file.
+ */
+(function Constants(window) {
+  "use strict";
+
+  window.appConstants = {};
+
+  // appConstants.CONSTANT_EXAMPLE = 'Example Value';
+
+})(window);
